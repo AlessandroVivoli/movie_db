4ddae1ac93c7a9b03ceb30ea1a31c26b822bbc48
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 13:09:05 +0100

    Added ProductionCompany model for MovieDetails

---
+++ lib/core/models/production_company/production_company.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'production_company.freezed.dart';
+part 'production_company.g.dart';
+
+@freezed
+class ProductionCompany with _$ProductionCompany {
+  @JsonSerializable(
+    explicitToJson: true,
+    fieldRename: FieldRename.snake,
+  )
+  const factory ProductionCompany({
+    required String name,
+    required int id,
+    required String? logoPath,
+    required String originCountry,
+  }) = _ProductionCompany;
+
+  factory ProductionCompany.fromJson(Map<String, Object?> json) =>
+      _$ProductionCompanyFromJson(json);
+}
