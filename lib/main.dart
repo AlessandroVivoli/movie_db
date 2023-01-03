fa452c7a1a557b82258244af9972da5ea7345101
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 19:52:47 +0100

    Fixed .env not loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_db/ui/pages/home/home.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
