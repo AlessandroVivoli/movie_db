ebe1fe22b61858f8f9e99b52b264cd8dbd5b650c
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 21:54:42 +0100

    TODO: MovieList

---
+++ lib/ui/shared/widgets/movie_list/movie_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class MovieList extends StatefulWidget {
+  const MovieList({super.key});
+
+  @override
+  State<MovieList> createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
