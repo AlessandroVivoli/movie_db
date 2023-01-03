287c3bc036df19feae25d86901e14279ce91a472
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 17:38:47 +0100

    Added Genre service to fetch genres

---
+++ lib/core/services/genre_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_db/core/models/genre/genre.dart';
+
+class GenreService {
+  static Future<List<Genre>?> getGenres() async {
+    try {
+      Response<dynamic> response = await Dio(
+        BaseOptions(queryParameters: {
+          "key": dotenv.env['TMDB_API_KEY'],
+          "lang": 'en-US'
+        }),
+      ).fetch(RequestOptions(
+        path: 'https://api.themoviedb.org/3/genre/movie/list',
+      ));
+
+      List<Genre> data = (response.data as List<Map<String, Object?>>)
+          .map((json) => Genre.fromJson(json))
+          .toList();
+
+      return data;
+    } on DioError {
+      return null;
+    }
+  }
+}
