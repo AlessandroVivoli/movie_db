ea7182ce8e1c938d5216a37eee3e47ad5a16de45
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 13:09:21 +0100

    Added ProductionCountry for MovieDetails

---
+++ lib/core/models/production_country/production_country.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'production_country.freezed.dart';
+part 'production_country.g.dart';
+
+@freezed
+class ProductionCountry with _$ProductionCountry {
+  @JsonSerializable(
+    explicitToJson: true,
+  )
+  const factory ProductionCountry({
+    required String iso_3166_1,
+    required String name,
+  }) = _ProductionCountry;
+
+  factory ProductionCountry.fromJson(Map<String, Object?> json) =>
+      _$ProductionCountryFromJson(json);
+}
