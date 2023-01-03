7c8a061f48b41afbdbcd2209c693f769d7b034d8
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-03 13:09:35 +0100

    Added SpokenLanguage model for MovieDetails

---
+++ lib/core/models/spoken_language/spoken_language.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'spoken_language.freezed.dart';
+part 'spoken_language.g.dart';
+
+@freezed
+class SpokenLanguage with _$SpokenLanguage {
+  @JsonSerializable(
+    explicitToJson: true,
+  )
+  const factory SpokenLanguage({
+    required String iso_649_1,
+    required String name,
+  }) = _SpokenLanguage;
+
+  factory SpokenLanguage.fromJson(Map<String, Object?> json) =>
+      _$SpokenLanguageFromJson(json);
+}
