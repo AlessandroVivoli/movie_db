fe24e26b01748b7f1ecf9bd033068e7ad3912952
Alessandro <alevivoli85@gmail.com>
2023-01-05 20:27:16 +0100

    Replaced FutureBuilder with seperated ListView

--- lib/ui/shared/widgets/movie_list/movie_list.dart
+++ lib/ui/shared/widgets/movie_list/movie_list.dart
@@ -4,42 +4,25 @@ import '../../../../core/models/movie/movie.dart';
 import '../movie_card/movie_card.dart';
 
 class MovieList extends StatelessWidget {
-  final Future<List<Movie>?>? movieList;
+  final List<Movie> movieList;
+  final double padding;
 
-  const MovieList({super.key, this.movieList});
+  const MovieList({super.key, required this.movieList, this.padding = 0});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: movieList,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasData == false) {
-            return const Center(
-              child: Text('Nothing found.'),
-            );
-          }
-
-          final data = snapshot.data!;
-
-          return ListView(
-            scrollDirection: Axis.horizontal,
-            children: List<Widget>.generate(
-              data.length,
-              (index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: MovieCard(
-                    movie: data[index],
-                  ),
-                );
-              },
-            ),
-          );
-        }
-
-        return const Center(
-          child: CircularProgressIndicator(),
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: movieList.length,
+      padding: EdgeInsets.symmetric(horizontal: padding),
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          width: 10,
+        );
+      },
+      itemBuilder: (context, index) {
+        return MovieCard(
+          movie: movieList[index],
         );
       },
     );
