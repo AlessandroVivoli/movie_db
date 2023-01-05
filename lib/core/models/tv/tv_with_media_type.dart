88888f207fdd0c3d191e673aa03d7907a25c4a79
Alessandro <alevivoli85@gmail.com>
2023-01-05 13:32:14 +0100

    Added TVWithMediaType model

---
+++ lib/core/models/tv/tv_with_media_type.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'tv_with_media_type.freezed.dart';
+part 'tv_with_media_type.g.dart';
+
+@freezed
+class TVWithMediaType with _$TVWithMediaType {
+  const factory TVWithMediaType({
+    required String? posterPath,
+    required double popularity,
+    required int id,
+    required String overview,
+    required String? backdropPath,
+    required double voteAverage,
+    required String mediaType,
+    required DateTime firstAirDate,
+    required List<String> originCountry,
+    required List<int> genreIds,
+    required String originalLanguage,
+    required int voteCount,
+    required String name,
+    required String originalName,
+  }) = _TVWithMediaType;
+
+  factory TVWithMediaType.fromJson(Map<String, Object?> json) =>
+      _$TVWithMediaTypeFromJson(json);
+}
