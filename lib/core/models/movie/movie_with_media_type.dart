2c54f73c4e8491062338c0fbdd48110090551c84
Alessandro <alevivoli85@gmail.com>
2023-01-05 13:32:04 +0100

    Added MovieWithMediaType model

---
+++ lib/core/models/movie/movie_with_media_type.dart
@@ -0,0 +1,30 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../genre/genre.dart';
+
+part 'movie_with_media_type.freezed.dart';
+part 'movie_with_media_type.g.dart';
+
+@freezed
+class MovieWithMediaType with _$MovieWithMediaType {
+  const factory MovieWithMediaType({
+    required String? posterPath,
+    required bool adult,
+    required String overview,
+    required DateTime releaseDate,
+    required String originalTitle,
+    required List<int> genreIds,
+    required int id,
+    required String mediaType,
+    required String originalLanguage,
+    required String title,
+    required String? backdropPath,
+    required double popularity,
+    required int voteCount,
+    required bool video,
+    required double voteAverage,
+  }) = _MovieWithMediaType;
+
+  factory MovieWithMediaType.fromJson(Map<String, Object?> json) =>
+      _$MovieWithMediaTypeFromJson(json);
+}
---
+++ lib/core/models/movie/movie_with_media_type.freezed.dart
@@ -0,0 +1,469 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'movie_with_media_type.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+MovieWithMediaType _$MovieWithMediaTypeFromJson(Map<String, dynamic> json) {
+  return _MovieWithMediaType.fromJson(json);
+}
+
+/// @nodoc
+mixin _$MovieWithMediaType {
+  String? get posterPath => throw _privateConstructorUsedError;
+  bool get adult => throw _privateConstructorUsedError;
+  String get overview => throw _privateConstructorUsedError;
+  DateTime get releaseDate => throw _privateConstructorUsedError;
+  String get originalTitle => throw _privateConstructorUsedError;
+  List<int> get genreIds => throw _privateConstructorUsedError;
+  int get id => throw _privateConstructorUsedError;
+  String get mediaType => throw _privateConstructorUsedError;
+  String get originalLanguage => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String? get backdropPath => throw _privateConstructorUsedError;
+  double get popularity => throw _privateConstructorUsedError;
+  int get voteCount => throw _privateConstructorUsedError;
+  bool get video => throw _privateConstructorUsedError;
+  double get voteAverage => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $MovieWithMediaTypeCopyWith<MovieWithMediaType> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MovieWithMediaTypeCopyWith<$Res> {
+  factory $MovieWithMediaTypeCopyWith(
+          MovieWithMediaType value, $Res Function(MovieWithMediaType) then) =
+      _$MovieWithMediaTypeCopyWithImpl<$Res, MovieWithMediaType>;
+  @useResult
+  $Res call(
+      {String? posterPath,
+      bool adult,
+      String overview,
+      DateTime releaseDate,
+      String originalTitle,
+      List<int> genreIds,
+      int id,
+      String mediaType,
+      String originalLanguage,
+      String title,
+      String? backdropPath,
+      double popularity,
+      int voteCount,
+      bool video,
+      double voteAverage});
+}
+
+/// @nodoc
+class _$MovieWithMediaTypeCopyWithImpl<$Res, $Val extends MovieWithMediaType>
+    implements $MovieWithMediaTypeCopyWith<$Res> {
+  _$MovieWithMediaTypeCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? posterPath = freezed,
+    Object? adult = null,
+    Object? overview = null,
+    Object? releaseDate = null,
+    Object? originalTitle = null,
+    Object? genreIds = null,
+    Object? id = null,
+    Object? mediaType = null,
+    Object? originalLanguage = null,
+    Object? title = null,
+    Object? backdropPath = freezed,
+    Object? popularity = null,
+    Object? voteCount = null,
+    Object? video = null,
+    Object? voteAverage = null,
+  }) {
+    return _then(_value.copyWith(
+      posterPath: freezed == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String?,
+      adult: null == adult
+          ? _value.adult
+          : adult // ignore: cast_nullable_to_non_nullable
+              as bool,
+      overview: null == overview
+          ? _value.overview
+          : overview // ignore: cast_nullable_to_non_nullable
+              as String,
+      releaseDate: null == releaseDate
+          ? _value.releaseDate
+          : releaseDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      originalTitle: null == originalTitle
+          ? _value.originalTitle
+          : originalTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      genreIds: null == genreIds
+          ? _value.genreIds
+          : genreIds // ignore: cast_nullable_to_non_nullable
+              as List<int>,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      mediaType: null == mediaType
+          ? _value.mediaType
+          : mediaType // ignore: cast_nullable_to_non_nullable
+              as String,
+      originalLanguage: null == originalLanguage
+          ? _value.originalLanguage
+          : originalLanguage // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      backdropPath: freezed == backdropPath
+          ? _value.backdropPath
+          : backdropPath // ignore: cast_nullable_to_non_nullable
+              as String?,
+      popularity: null == popularity
+          ? _value.popularity
+          : popularity // ignore: cast_nullable_to_non_nullable
+              as double,
+      voteCount: null == voteCount
+          ? _value.voteCount
+          : voteCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      video: null == video
+          ? _value.video
+          : video // ignore: cast_nullable_to_non_nullable
+              as bool,
+      voteAverage: null == voteAverage
+          ? _value.voteAverage
+          : voteAverage // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_MovieWithMediaTypeCopyWith<$Res>
+    implements $MovieWithMediaTypeCopyWith<$Res> {
+  factory _$$_MovieWithMediaTypeCopyWith(_$_MovieWithMediaType value,
+          $Res Function(_$_MovieWithMediaType) then) =
+      __$$_MovieWithMediaTypeCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? posterPath,
+      bool adult,
+      String overview,
+      DateTime releaseDate,
+      String originalTitle,
+      List<int> genreIds,
+      int id,
+      String mediaType,
+      String originalLanguage,
+      String title,
+      String? backdropPath,
+      double popularity,
+      int voteCount,
+      bool video,
+      double voteAverage});
+}
+
+/// @nodoc
+class __$$_MovieWithMediaTypeCopyWithImpl<$Res>
+    extends _$MovieWithMediaTypeCopyWithImpl<$Res, _$_MovieWithMediaType>
+    implements _$$_MovieWithMediaTypeCopyWith<$Res> {
+  __$$_MovieWithMediaTypeCopyWithImpl(
+      _$_MovieWithMediaType _value, $Res Function(_$_MovieWithMediaType) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? posterPath = freezed,
+    Object? adult = null,
+    Object? overview = null,
+    Object? releaseDate = null,
+    Object? originalTitle = null,
+    Object? genreIds = null,
+    Object? id = null,
+    Object? mediaType = null,
+    Object? originalLanguage = null,
+    Object? title = null,
+    Object? backdropPath = freezed,
+    Object? popularity = null,
+    Object? voteCount = null,
+    Object? video = null,
+    Object? voteAverage = null,
+  }) {
+    return _then(_$_MovieWithMediaType(
+      posterPath: freezed == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String?,
+      adult: null == adult
+          ? _value.adult
+          : adult // ignore: cast_nullable_to_non_nullable
+              as bool,
+      overview: null == overview
+          ? _value.overview
+          : overview // ignore: cast_nullable_to_non_nullable
+              as String,
+      releaseDate: null == releaseDate
+          ? _value.releaseDate
+          : releaseDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      originalTitle: null == originalTitle
+          ? _value.originalTitle
+          : originalTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      genreIds: null == genreIds
+          ? _value._genreIds
+          : genreIds // ignore: cast_nullable_to_non_nullable
+              as List<int>,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      mediaType: null == mediaType
+          ? _value.mediaType
+          : mediaType // ignore: cast_nullable_to_non_nullable
+              as String,
+      originalLanguage: null == originalLanguage
+          ? _value.originalLanguage
+          : originalLanguage // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      backdropPath: freezed == backdropPath
+          ? _value.backdropPath
+          : backdropPath // ignore: cast_nullable_to_non_nullable
+              as String?,
+      popularity: null == popularity
+          ? _value.popularity
+          : popularity // ignore: cast_nullable_to_non_nullable
+              as double,
+      voteCount: null == voteCount
+          ? _value.voteCount
+          : voteCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      video: null == video
+          ? _value.video
+          : video // ignore: cast_nullable_to_non_nullable
+              as bool,
+      voteAverage: null == voteAverage
+          ? _value.voteAverage
+          : voteAverage // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_MovieWithMediaType implements _MovieWithMediaType {
+  const _$_MovieWithMediaType(
+      {required this.posterPath,
+      required this.adult,
+      required this.overview,
+      required this.releaseDate,
+      required this.originalTitle,
+      required final List<int> genreIds,
+      required this.id,
+      required this.mediaType,
+      required this.originalLanguage,
+      required this.title,
+      required this.backdropPath,
+      required this.popularity,
+      required this.voteCount,
+      required this.video,
+      required this.voteAverage})
+      : _genreIds = genreIds;
+
+  factory _$_MovieWithMediaType.fromJson(Map<String, dynamic> json) =>
+      _$$_MovieWithMediaTypeFromJson(json);
+
+  @override
+  final String? posterPath;
+  @override
+  final bool adult;
+  @override
+  final String overview;
+  @override
+  final DateTime releaseDate;
+  @override
+  final String originalTitle;
+  final List<int> _genreIds;
+  @override
+  List<int> get genreIds {
+    if (_genreIds is EqualUnmodifiableListView) return _genreIds;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_genreIds);
+  }
+
+  @override
+  final int id;
+  @override
+  final String mediaType;
+  @override
+  final String originalLanguage;
+  @override
+  final String title;
+  @override
+  final String? backdropPath;
+  @override
+  final double popularity;
+  @override
+  final int voteCount;
+  @override
+  final bool video;
+  @override
+  final double voteAverage;
+
+  @override
+  String toString() {
+    return 'MovieWithMediaType(posterPath: $posterPath, adult: $adult, overview: $overview, releaseDate: $releaseDate, originalTitle: $originalTitle, genreIds: $genreIds, id: $id, mediaType: $mediaType, originalLanguage: $originalLanguage, title: $title, backdropPath: $backdropPath, popularity: $popularity, voteCount: $voteCount, video: $video, voteAverage: $voteAverage)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_MovieWithMediaType &&
+            (identical(other.posterPath, posterPath) ||
+                other.posterPath == posterPath) &&
+            (identical(other.adult, adult) || other.adult == adult) &&
+            (identical(other.overview, overview) ||
+                other.overview == overview) &&
+            (identical(other.releaseDate, releaseDate) ||
+                other.releaseDate == releaseDate) &&
+            (identical(other.originalTitle, originalTitle) ||
+                other.originalTitle == originalTitle) &&
+            const DeepCollectionEquality().equals(other._genreIds, _genreIds) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.mediaType, mediaType) ||
+                other.mediaType == mediaType) &&
+            (identical(other.originalLanguage, originalLanguage) ||
+                other.originalLanguage == originalLanguage) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.backdropPath, backdropPath) ||
+                other.backdropPath == backdropPath) &&
+            (identical(other.popularity, popularity) ||
+                other.popularity == popularity) &&
+            (identical(other.voteCount, voteCount) ||
+                other.voteCount == voteCount) &&
+            (identical(other.video, video) || other.video == video) &&
+            (identical(other.voteAverage, voteAverage) ||
+                other.voteAverage == voteAverage));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      posterPath,
+      adult,
+      overview,
+      releaseDate,
+      originalTitle,
+      const DeepCollectionEquality().hash(_genreIds),
+      id,
+      mediaType,
+      originalLanguage,
+      title,
+      backdropPath,
+      popularity,
+      voteCount,
+      video,
+      voteAverage);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_MovieWithMediaTypeCopyWith<_$_MovieWithMediaType> get copyWith =>
+      __$$_MovieWithMediaTypeCopyWithImpl<_$_MovieWithMediaType>(
+          this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_MovieWithMediaTypeToJson(
+      this,
+    );
+  }
+}
+
+abstract class _MovieWithMediaType implements MovieWithMediaType {
+  const factory _MovieWithMediaType(
+      {required final String? posterPath,
+      required final bool adult,
+      required final String overview,
+      required final DateTime releaseDate,
+      required final String originalTitle,
+      required final List<int> genreIds,
+      required final int id,
+      required final String mediaType,
+      required final String originalLanguage,
+      required final String title,
+      required final String? backdropPath,
+      required final double popularity,
+      required final int voteCount,
+      required final bool video,
+      required final double voteAverage}) = _$_MovieWithMediaType;
+
+  factory _MovieWithMediaType.fromJson(Map<String, dynamic> json) =
+      _$_MovieWithMediaType.fromJson;
+
+  @override
+  String? get posterPath;
+  @override
+  bool get adult;
+  @override
+  String get overview;
+  @override
+  DateTime get releaseDate;
+  @override
+  String get originalTitle;
+  @override
+  List<int> get genreIds;
+  @override
+  int get id;
+  @override
+  String get mediaType;
+  @override
+  String get originalLanguage;
+  @override
+  String get title;
+  @override
+  String? get backdropPath;
+  @override
+  double get popularity;
+  @override
+  int get voteCount;
+  @override
+  bool get video;
+  @override
+  double get voteAverage;
+  @override
+  @JsonKey(ignore: true)
+  _$$_MovieWithMediaTypeCopyWith<_$_MovieWithMediaType> get copyWith =>
+      throw _privateConstructorUsedError;
+}
