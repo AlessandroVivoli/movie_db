88daf235837d871ba606e78f046e2adaf5e254b6
Alessandro <alevivoli85@gmail.com>
2023-01-05 22:51:25 +0100

    Updated to use BlankProfileImage instead of NetworkImage

--- lib/ui/shared/widgets/person_card/person_card.dart
+++ lib/ui/shared/widgets/person_card/person_card.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/models/person/person.dart';
+import '../blank_profile_image/blank_profile_image.dart';
 
 class PersonCard extends StatelessWidget {
   final Person person;
+  final double imgRadius;
+  final double imgBorderWidth;
 
-  const PersonCard({super.key, required this.person});
+  const PersonCard({
+    super.key,
+    required this.person,
+    this.imgRadius = 40,
+    this.imgBorderWidth = 2,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final path = (person.profilePath != null)
-        ? 'https://image.tmdb.org/t/p/w500/${person.profilePath!}'
-        : 'https://images.unsplash.com/photo-1511367461989-f85a21fda167';
+    final imgPath = 'https://image.tmdb.org/t/p/w500/${person.profilePath}';
 
     return GestureDetector(
       onTap: () {
@@ -19,14 +25,20 @@ class PersonCard extends StatelessWidget {
       },
       child: Column(
         children: [
-          Center(
-            child: CircleAvatar(
-              radius: 40,
-              backgroundImage: NetworkImage(
-                path,
+          if (person.profilePath != null)
+            Center(
+              child: CircleAvatar(
+                radius: imgRadius,
+                backgroundImage: NetworkImage(
+                  imgPath,
+                ),
               ),
             ),
-          ),
+          if (person.profilePath == null)
+            BlankProfileImage(
+              imgRadius: imgRadius,
+              imgBorderWidth: imgBorderWidth,
+            ),
           Container(
             margin: const EdgeInsets.only(top: 8),
             child: Column(
