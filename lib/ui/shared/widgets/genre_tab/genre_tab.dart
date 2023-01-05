c6f95150fb7481a7c2bef347e78bd9797162c7b1
Alessandro <alevivoli85@gmail.com>
2023-01-05 20:26:46 +0100

    Rearranged conditions

--- lib/ui/shared/widgets/genre_tab/genre_tab.dart
+++ lib/ui/shared/widgets/genre_tab/genre_tab.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/models/movie/movie.dart';
 import '../../../../core/services/genre_service.dart';
 import '../../../../core/services/movie_service.dart';
 import '../movie_list/movie_list.dart';
@@ -12,59 +14,98 @@ class GenreTab extends StatelessWidget {
     return FutureBuilder(
       future: GenreService.getGenres(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasData == false) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [Text('Nothing found')],
-            );
-          }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [CircularProgressIndicator()],
+          );
+        }
 
-          final data = snapshot.data!;
+        if (snapshot.hasError) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [Text('Nothing found')],
+          );
+        }
 
-          return DefaultTabController(
-            length: data.length,
-            child: Column(
-              children: [
-                TabBar(
-                  isScrollable: true,
-                  tabs: List<Tab>.generate(
-                    data.length,
-                    (index) {
-                      return Tab(
-                        child: Text(data[index].name),
-                      );
-                    },
-                  ),
+        if (!snapshot.hasData || (snapshot.hasData && snapshot.data!.isEmpty)) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [Text('Nothing found')],
+          );
+        }
+
+        final data = snapshot.data!;
+
+        return DefaultTabController(
+          length: data.length,
+          child: Column(
+            children: [
+              TabBar(
+                isScrollable: true,
+                tabs: List<Tab>.generate(
+                  data.length,
+                  (index) {
+                    return Tab(
+                      child: Text(data[index].name),
+                    );
+                  },
                 ),
-                SizedBox(
-                  height: 300,
-                  child: TabBarView(
-                    children: List<Widget>.generate(
-                      data.length,
-                      (index) => Container(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        child: MovieList(
-                          movieList: MovieService.getPopularMovies(
-                            withGenres: [data[index].id],
-                          ),
+              ),
+              LimitedBox(
+                maxHeight: 250,
+                child: TabBarView(
+                  children: List<Widget>.generate(
+                    data.length,
+                    (index) => Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: FutureBuilder(
+                        future: MovieService.getTrendingMovies(
+                          withGenres: [data[index].id],
                         ),
+                        builder: _buildMovieList,
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-          );
-        }
-
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [CircularProgressIndicator()],
+              ),
+            ],
+          ),
         );
       },
+    );
+  }
+
+  Widget _buildMovieList(
+    BuildContext context,
+    AsyncSnapshot<List<Movie>> snapshot,
+  ) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (snapshot.hasError) {
+      debugPrint((snapshot.error as DioError).message);
+
+      return const Center(
+        child: Text('Nothing found!'),
+      );
+    }
+
+    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+      return const Center(
+        child: Text('Nothing found!'),
+      );
+    }
+
+    return MovieList(
+      movieList: snapshot.data!,
+      padding: 10,
     );
   }
 }
