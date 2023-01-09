19c9900b4bb31efa0efe313926aa8193cdfea869
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-09 20:54:11 +0100

    Added joinElement extenstion on List

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -19,3 +19,13 @@ extension ListContains<T> on List<T> {
     return false;
   }
 }
+
+extension ListJoin<T> on List<T> {
+  List<T> joinElement(T element) {
+    for (int i = 1; i < length; i += 2) {
+      insert(i, element);
+    }
+
+    return this;
+  }
+}
