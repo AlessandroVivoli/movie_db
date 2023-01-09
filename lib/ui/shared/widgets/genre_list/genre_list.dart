9e211d34de3a0410c2fcc0fca940f6bef2ce9f31
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-09 20:53:52 +0100

    Added GenreList widget

---
+++ lib/ui/shared/widgets/genre_list/genre_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/models/genre/genre.dart';
+import '../../../../utils/extensions.dart';
+
+class GenreList extends StatelessWidget {
+  final List<Genre> genres;
+
+  const GenreList({super.key, required this.genres});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List<Widget>.generate(
+        genres.length,
+        (index) => Container(
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            border: Border.fromBorderSide(
+              BorderSide(
+                width: 2,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+          ),
+          child: Text(
+            genres[index].name,
+            style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+          ),
+        ),
+      ).joinElement(
+        const SizedBox(
+          width: 10,
+        ),
+      ),
+    );
+  }
+}
