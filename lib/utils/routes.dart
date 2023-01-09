2fc46c291f0836343cc3f9d0a8a2932ed5155272
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-09 20:54:17 +0100

    Updated routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,1 +1,9 @@
-//TODO: Add routes
+import 'package:flutter/cupertino.dart';
+
+import '../ui/pages/home/home.dart';
+import '../ui/pages/movie_details/movie_details.dart';
+
+final Map<String, Widget Function(BuildContext)> routes = {
+  '/home': (context) => const HomePage(title: 'FlutterMovie'),
+  '/details': (context) => const MovieDetailsPage(),
+};
