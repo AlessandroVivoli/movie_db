46150a33ecb61d21ef671998ede59082cd845713
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-09 20:53:02 +0100

    Addded getCast method

--- lib/core/services/person_service.dart
+++ lib/core/services/person_service.dart
@@ -11,4 +11,12 @@ class PersonService {
         .then((rawList) => rawList.map(Person.fromJson))
         .then((persons) => persons.toList());
   }
+
+  static Future<List<Person>> getCast({required int movieId}) {
+    return DioProvider.dio
+        .get('/movie/$movieId/credits')
+        .then((res) => List<Map<String, Object?>>.from(res.data['cast']))
+        .then((rawList) => rawList.map(Person.fromJson))
+        .then((persons) => persons.toList());
+  }
 }
