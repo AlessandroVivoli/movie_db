ea34baed2f913c6edd665971ed05a3be3deeb25e
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-10 16:39:41 +0100

    Added Video model

---
+++ lib/core/models/video/video.freezed.dart
@@ -0,0 +1,333 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'video.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Video _$VideoFromJson(Map<String, dynamic> json) {
+  return _Video.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Video {
+  String? get iso_649_1 => throw _privateConstructorUsedError;
+  String? get iso_3166_1 => throw _privateConstructorUsedError;
+  String? get name => throw _privateConstructorUsedError;
+  String? get key => throw _privateConstructorUsedError;
+  String? get site => throw _privateConstructorUsedError;
+  int get size => throw _privateConstructorUsedError;
+  String? get type => throw _privateConstructorUsedError;
+  bool get official => throw _privateConstructorUsedError;
+  String? get publishedAt => throw _privateConstructorUsedError;
+  String? get id => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $VideoCopyWith<Video> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $VideoCopyWith<$Res> {
+  factory $VideoCopyWith(Video value, $Res Function(Video) then) =
+      _$VideoCopyWithImpl<$Res, Video>;
+  @useResult
+  $Res call(
+      {String? iso_649_1,
+      String? iso_3166_1,
+      String? name,
+      String? key,
+      String? site,
+      int size,
+      String? type,
+      bool official,
+      String? publishedAt,
+      String? id});
+}
+
+/// @nodoc
+class _$VideoCopyWithImpl<$Res, $Val extends Video>
+    implements $VideoCopyWith<$Res> {
+  _$VideoCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? iso_649_1 = freezed,
+    Object? iso_3166_1 = freezed,
+    Object? name = freezed,
+    Object? key = freezed,
+    Object? site = freezed,
+    Object? size = null,
+    Object? type = freezed,
+    Object? official = null,
+    Object? publishedAt = freezed,
+    Object? id = freezed,
+  }) {
+    return _then(_value.copyWith(
+      iso_649_1: freezed == iso_649_1
+          ? _value.iso_649_1
+          : iso_649_1 // ignore: cast_nullable_to_non_nullable
+              as String?,
+      iso_3166_1: freezed == iso_3166_1
+          ? _value.iso_3166_1
+          : iso_3166_1 // ignore: cast_nullable_to_non_nullable
+              as String?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      key: freezed == key
+          ? _value.key
+          : key // ignore: cast_nullable_to_non_nullable
+              as String?,
+      site: freezed == site
+          ? _value.site
+          : site // ignore: cast_nullable_to_non_nullable
+              as String?,
+      size: null == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as int,
+      type: freezed == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String?,
+      official: null == official
+          ? _value.official
+          : official // ignore: cast_nullable_to_non_nullable
+              as bool,
+      publishedAt: freezed == publishedAt
+          ? _value.publishedAt
+          : publishedAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_VideoCopyWith<$Res> implements $VideoCopyWith<$Res> {
+  factory _$$_VideoCopyWith(_$_Video value, $Res Function(_$_Video) then) =
+      __$$_VideoCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? iso_649_1,
+      String? iso_3166_1,
+      String? name,
+      String? key,
+      String? site,
+      int size,
+      String? type,
+      bool official,
+      String? publishedAt,
+      String? id});
+}
+
+/// @nodoc
+class __$$_VideoCopyWithImpl<$Res> extends _$VideoCopyWithImpl<$Res, _$_Video>
+    implements _$$_VideoCopyWith<$Res> {
+  __$$_VideoCopyWithImpl(_$_Video _value, $Res Function(_$_Video) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? iso_649_1 = freezed,
+    Object? iso_3166_1 = freezed,
+    Object? name = freezed,
+    Object? key = freezed,
+    Object? site = freezed,
+    Object? size = null,
+    Object? type = freezed,
+    Object? official = null,
+    Object? publishedAt = freezed,
+    Object? id = freezed,
+  }) {
+    return _then(_$_Video(
+      iso_649_1: freezed == iso_649_1
+          ? _value.iso_649_1
+          : iso_649_1 // ignore: cast_nullable_to_non_nullable
+              as String?,
+      iso_3166_1: freezed == iso_3166_1
+          ? _value.iso_3166_1
+          : iso_3166_1 // ignore: cast_nullable_to_non_nullable
+              as String?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      key: freezed == key
+          ? _value.key
+          : key // ignore: cast_nullable_to_non_nullable
+              as String?,
+      site: freezed == site
+          ? _value.site
+          : site // ignore: cast_nullable_to_non_nullable
+              as String?,
+      size: null == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as int,
+      type: freezed == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String?,
+      official: null == official
+          ? _value.official
+          : official // ignore: cast_nullable_to_non_nullable
+              as bool,
+      publishedAt: freezed == publishedAt
+          ? _value.publishedAt
+          : publishedAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Video implements _Video {
+  const _$_Video(
+      {required this.iso_649_1,
+      required this.iso_3166_1,
+      required this.name,
+      required this.key,
+      required this.site,
+      required this.size,
+      required this.type,
+      required this.official,
+      required this.publishedAt,
+      required this.id});
+
+  factory _$_Video.fromJson(Map<String, dynamic> json) =>
+      _$$_VideoFromJson(json);
+
+  @override
+  final String? iso_649_1;
+  @override
+  final String? iso_3166_1;
+  @override
+  final String? name;
+  @override
+  final String? key;
+  @override
+  final String? site;
+  @override
+  final int size;
+  @override
+  final String? type;
+  @override
+  final bool official;
+  @override
+  final String? publishedAt;
+  @override
+  final String? id;
+
+  @override
+  String toString() {
+    return 'Video(iso_649_1: $iso_649_1, iso_3166_1: $iso_3166_1, name: $name, key: $key, site: $site, size: $size, type: $type, official: $official, publishedAt: $publishedAt, id: $id)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Video &&
+            (identical(other.iso_649_1, iso_649_1) ||
+                other.iso_649_1 == iso_649_1) &&
+            (identical(other.iso_3166_1, iso_3166_1) ||
+                other.iso_3166_1 == iso_3166_1) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.key, key) || other.key == key) &&
+            (identical(other.site, site) || other.site == site) &&
+            (identical(other.size, size) || other.size == size) &&
+            (identical(other.type, type) || other.type == type) &&
+            (identical(other.official, official) ||
+                other.official == official) &&
+            (identical(other.publishedAt, publishedAt) ||
+                other.publishedAt == publishedAt) &&
+            (identical(other.id, id) || other.id == id));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, iso_649_1, iso_3166_1, name, key,
+      site, size, type, official, publishedAt, id);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_VideoCopyWith<_$_Video> get copyWith =>
+      __$$_VideoCopyWithImpl<_$_Video>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_VideoToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Video implements Video {
+  const factory _Video(
+      {required final String? iso_649_1,
+      required final String? iso_3166_1,
+      required final String? name,
+      required final String? key,
+      required final String? site,
+      required final int size,
+      required final String? type,
+      required final bool official,
+      required final String? publishedAt,
+      required final String? id}) = _$_Video;
+
+  factory _Video.fromJson(Map<String, dynamic> json) = _$_Video.fromJson;
+
+  @override
+  String? get iso_649_1;
+  @override
+  String? get iso_3166_1;
+  @override
+  String? get name;
+  @override
+  String? get key;
+  @override
+  String? get site;
+  @override
+  int get size;
+  @override
+  String? get type;
+  @override
+  bool get official;
+  @override
+  String? get publishedAt;
+  @override
+  String? get id;
+  @override
+  @JsonKey(ignore: true)
+  _$$_VideoCopyWith<_$_Video> get copyWith =>
+      throw _privateConstructorUsedError;
+}
