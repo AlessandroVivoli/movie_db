0efb6746495cd0710d583e1e1b89d9d8cbd5c6f1
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-10 16:41:06 +0100

    Added video player widget

---
+++ lib/ui/shared/pages/play/player/player.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+import '../../../../../core/models/video/video.dart';
+
+class Player extends StatefulWidget {
+  final Video video;
+
+  const Player({super.key, required this.video});
+
+  @override
+  State<Player> createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  late final YoutubePlayerController _controller;
+
+  late Orientation _currentOrientation;
+
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(initialVideoId: widget.video.key!);
+
+    _currentOrientation = Orientation.portrait;
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: YoutubePlayer(
+        aspectRatio: 16 / 9,
+        controller: _controller,
+        bottomActions: const [],
+        topActions: const [],
+      ),
+    );
+  }
+}
