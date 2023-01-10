ab0eadd93f5a5e2b2415917734a08a6af30a53b1
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-10 16:41:54 +0100

    Added video player route

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 import '../ui/pages/home/home.dart';
-import '../ui/pages/movie_details/movie_details.dart';
+import '../ui/shared/pages/movie_details/movie_details.dart';
+import '../ui/shared/pages/play/play.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   '/home': (context) => const HomePage(title: 'FlutterMovie'),
   '/details': (context) => const MovieDetailsPage(),
+  '/play': (context) => const PlayPage(),
 };
