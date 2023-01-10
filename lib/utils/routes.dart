6f78b3d4272fa606245fe316942588b4be557e1f
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-10 19:40:09 +0100

    Updated routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-import '../ui/pages/home/home.dart';
+import '../ui/pages/home_screen/home_screen.dart';
 import '../ui/shared/pages/movie_details/movie_details.dart';
-import '../ui/shared/pages/play/play.dart';
+import '../ui/shared/pages/player_screen/player_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
-  '/home': (context) => const HomePage(title: 'FlutterMovie'),
+  '/home': (context) => const HomeScreen(title: 'FlutterMovie'),
   '/details': (context) => const MovieDetailsPage(),
-  '/play': (context) => const PlayPage(),
+  '/play': (context) => const PlayerScreen(),
 };
