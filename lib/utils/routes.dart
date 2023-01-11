fee9fd02a2f955afc477b059d57371886d7e7db2
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-11 16:32:11 +0100

    Removed routes that need to be generated

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 import '../ui/pages/home_screen/home_screen.dart';
-import '../ui/pages/movie_details_screen/movie_details_screen.dart';
-import '../ui/pages/player_screen/player_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   '/home': (context) => const HomeScreen(title: 'FlutterMovie'),
-  '/details': (context) => const MovieDetailsScreen(),
-  '/play': (context) => const PlayerScreen(),
 };
