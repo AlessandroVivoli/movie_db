5948cef2dcffbd14520465f72e0fe4c6d0fcacee
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-11 17:56:15 +0100

    Added RouteNames enum

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -5,3 +5,12 @@ enum LogoSizes { w45, w92, w154, w185, w300, w500, original }
 enum PosterSizes { w92, w154, w185, w342, w500, w780, original }
 
 enum ProfileSizes { w45, w185, h632, original }
+
+enum RouteNames {
+  movieDetails('details'),
+  player('play');
+
+  final String name;
+
+  const RouteNames(String name) : name = '/$name';
+}
