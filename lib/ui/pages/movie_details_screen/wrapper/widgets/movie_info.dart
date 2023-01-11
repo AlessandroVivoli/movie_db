76eb467ab13417932ff2bda3c537a159addaabfb
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-11 15:16:59 +0100

    Simplifed MovieDetailsScreen widget - Extracted smaller widgets into seperated files

---
+++ lib/ui/pages/movie_details_screen/wrapper/widgets/movie_info.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class MovieInfo extends StatelessWidget {
+  final int budget;
+  final int runtime;
+  final String releaseDate;
+
+  const MovieInfo({
+    super.key,
+    required this.budget,
+    required this.runtime,
+    required this.releaseDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _BudgetText(budget: budget),
+        _DurationText(runtime: runtime),
+        _ReleaseDateText(releaseDate: releaseDate),
+      ],
+    );
+  }
+}
+
+class _BudgetText extends StatelessWidget {
+  const _BudgetText({
+    Key? key,
+    required this.budget,
+  }) : super(key: key);
+
+  final int budget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Budget'.toUpperCase(),
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.secondary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          '$budget \$',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _DurationText extends StatelessWidget {
+  const _DurationText({
+    Key? key,
+    required this.runtime,
+  }) : super(key: key);
+
+  final int runtime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Duration'.toUpperCase(),
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.secondary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          '$runtime min',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ReleaseDateText extends StatelessWidget {
+  const _ReleaseDateText({
+    Key? key,
+    required this.releaseDate,
+  }) : super(key: key);
+
+  final String releaseDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Release date'.toUpperCase(),
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.secondary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          releaseDate,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
