1c55576d24eed1fa017d273784a39db0793e8f6a
Alessandro <alevivoli85@gmail.com>
2023-01-13 16:22:44 +0100

    Added CustomNetworkImage widget

---
+++ lib/ui/shared/widgets/custom_image/custom_network_image.dart
@@ -0,0 +1,43 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage({
+    super.key,
+    this.url,
+    required this.placeholderIcon,
+    this.width,
+    this.height,
+  });
+
+  final double? width;
+  final double? height;
+
+  final String? url;
+  final Icon placeholderIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    if (url != null) {
+      return SizedBox.expand(
+        child: CachedNetworkImage(
+          width: width,
+          height: height,
+          fit: BoxFit.cover,
+          imageUrl: url!,
+          progressIndicatorBuilder: (context, url, download) => Center(
+            child: CircularProgressIndicator(
+              value: download.progress,
+            ),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+      );
+    }
+    return Icon(
+      placeholderIcon.icon,
+      color: Colors.white,
+      size: placeholderIcon.size,
+    );
+  }
+}
