4f9437db5215881e101515d1416f6b6fed54b4a6
Alessandro <alevivoli85@gmail.com>
2023-01-13 16:26:59 +0100

    Updated MovieCard to use CustomNetworkImage - Changed from unnecessary container to padding

--- lib/ui/shared/widgets/movie_card/movie_card.dart
+++ lib/ui/shared/widgets/movie_card/movie_card.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/models/movie/movie.dart';
@@ -6,6 +5,7 @@ import '../../../../core/services/image_service.dart';
 import '../../../../core/services/movie_service.dart';
 import '../../../../utils/enums.dart';
 import '../../../../utils/routes.dart';
+import '../custom_image/custom_network_image.dart';
 import '../rating/rating.dart';
 
 class MovieCard extends StatelessWidget {
@@ -31,30 +31,23 @@ class MovieCard extends StatelessWidget {
               : MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (movie.posterPath != null)
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        ImageService.getImageUrl(
-                          size: PosterSizes.w500.name,
-                          path: movie.posterPath!,
-                        ),
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(5)),
-                  ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: CustomNetworkImage(
+                  url: ImageService.getImageUrl(
+                      size: PosterSizes.w500.name, path: movie.posterPath),
+                  placeholderIcon: const Icon(Icons.movie),
                 ),
               ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 10, bottom: 5),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 5),
                     child: Text(
                       '${movie.title}',
                       overflow: (movie.posterPath != null)
