544ea181a340f0fb04ef8c59ce64e634950d3ea6
Alessandro <alevivoli85@gmail.com>
2023-01-13 16:20:25 +0100

    Changed return value of method to optional

--- lib/core/services/image_service.dart
+++ lib/core/services/image_service.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ImageService {
-  static String getImageUrl({required String size, required String path}) {
+  static String? getImageUrl({required String size, required String? path}) {
+    if (path == null) return null;
+
     return '${dotenv.env['BASE_IMG_URL']}$size$path';
   }
 }
