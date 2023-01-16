3f8105309a75b6def5b06d24a5082d1c2ee3e000
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 20:15:13 +0100

    Changed to SliverAppBar - Updated FutureBuilder to match official example

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../core/services/movie_service.dart';
 import '../../shared/widgets/carousel/movie_carousel/movie_carousel.dart';
@@ -20,26 +21,37 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      top: false,
       child: Scaffold(
         drawer: const Drawer(),
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: const [
-              _TrendingMoviesBuilder(),
-              LimitedBox(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+              ),
+              title: Text(widget.title),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                ),
+              ],
+              expandedHeight: 200,
+              pinned: true,
+              flexibleSpace: const FlexibleSpaceBar(
+                background: _TrendingMoviesBuilder(),
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: LimitedBox(
                 maxHeight: 300,
                 child: GenreTab(),
               ),
-              HomeWrapper(),
-            ],
-          ),
+            ),
+            const SliverToBoxAdapter(child: HomeWrapper()),
+          ],
         ),
       ),
     );
@@ -56,50 +68,36 @@ class _TrendingMoviesBuilder extends StatelessWidget {
     return FutureBuilder(
       future: MovieService.getTrendingMovies(timeWindow: 'week'),
       builder: (context, snapshot) {
-        late Widget widget;
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          widget = const SizedBox(
-            height: 200,
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else {
-          if (snapshot.hasError) {
-            WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Theme.of(context).colorScheme.surface,
-                  content: const ErrorSnackBarContent(
-                    message: 'Could not get trending movies.',
-                  ),
-                ),
-              );
-            });
-
-            widget = const SizedBox(
-              height: 200,
-              child: Center(
-                child: ErrorText('Something went wrong.'),
-              ),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            widget = const SizedBox(
-              height: 200,
-              child: Center(
-                child: Text('Nothing found.'),
-              ),
-            );
-          } else {
-            widget = MovieCarousel(
-              height: 200,
-              movies: (snapshot.data!).take(6).toList(),
+        if (snapshot.hasData) {
+          if (snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text('Nothing found.'),
             );
           }
+
+          return MovieCarousel(
+            movies: (snapshot.data!).take(6).toList(),
+          );
+        } else if (snapshot.hasError) {
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Theme.of(context).colorScheme.surface,
+                content: const ErrorSnackBarContent(
+                  message: 'Could not get trending movies.',
+                ),
+              ),
+            );
+          });
+
+          return const Center(
+            child: ErrorText('Something went wrong.'),
+          );
         }
 
-        return widget;
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
