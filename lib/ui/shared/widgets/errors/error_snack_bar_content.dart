5e8c8d7bd4110b71798ad9c9625f5a3f5d367025
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 16:52:48 +0100

    Added ErrorSnackBarContent for SnackBar - Added ErrorText widget

---
+++ lib/ui/shared/widgets/errors/error_snack_bar_content.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ErrorSnackBarContent extends StatelessWidget {
+  const ErrorSnackBarContent({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Icon(
+          Icons.error_outline,
+          color: Colors.red,
+        ),
+        const SizedBox(width: 10),
+        Text(
+          message,
+          style: const TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.w600,
+          ),
+        )
+      ],
+    );
+  }
+}
