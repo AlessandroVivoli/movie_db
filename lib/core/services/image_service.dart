0e0c0a981b5036004f283f43fb69020d8d9dd7e2
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 16:51:41 +0100

    Removed MovieImages service - Moved getMovieImages method to new Image service - Added getPersonImages method

--- lib/core/services/image_service.dart
+++ lib/core/services/image_service.dart
@@ -1,9 +1,28 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import '../models/image/image_model.dart';
+import '../models/movie/image/movie_images.dart';
+import '../providers/dio_provider.dart';
+
 class ImageService {
   static String? getImageUrl({required String size, required String? path}) {
     if (path == null) return null;
 
     return '${dotenv.env['BASE_IMG_URL']}$size$path';
+  }
+
+  static Future<MovieImages> getMovieImages({required int id}) {
+    return DioProvider.dio
+        .get('/movie/$id/images')
+        .then((res) => Map<String, Object?>.from(res.data))
+        .then(MovieImages.fromJson);
+  }
+
+  static Future<List<ImageModel>> getPersonImages({required int id}) {
+    return DioProvider.dio
+        .get('/person/$id/images')
+        .then((res) => List<Map<String, Object?>>.from(res.data['profiles']))
+        .then((rawList) => rawList.map(ImageModel.fromJson))
+        .then((images) => images.toList());
   }
 }
