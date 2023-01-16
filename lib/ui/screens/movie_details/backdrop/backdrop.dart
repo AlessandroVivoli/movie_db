72f2366ecc51345bd7c7ec9bd2b84fe362dd8302
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 16:56:08 +0100

    Changed statusBarColor to transparent

--- lib/ui/screens/movie_details/backdrop/backdrop.dart
+++ lib/ui/screens/movie_details/backdrop/backdrop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../../core/models/movie/details/movie_details.dart';
 import '../../../shared/widgets/backdrop_image/backdrop_image.dart';
@@ -10,20 +11,35 @@ class MovieDetailsBackdrop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+
     return SliverAppBar(
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
       pinned: true,
       expandedHeight: 250,
       flexibleSpace: FlexibleSpaceBar(
-        title: Text(
-          details.originalTitle ?? '',
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(
-            fontSize: 16,
+        title: SizedBox(
+          height: 20,
+          width: (orientation == Orientation.portrait) ? 200 : 400,
+          child: Center(
+            child: Text(
+              details.originalTitle ?? '',
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
           ),
         ),
         centerTitle: true,
         background: BackdropImage(
           imgUrl: details.backdropPath,
+          placeholderIcon: const Icon(
+            Icons.movie,
+            size: 100,
+          ),
         ),
       ),
     );
