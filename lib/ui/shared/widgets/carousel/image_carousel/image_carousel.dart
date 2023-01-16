4b473c98e97d5df8f209259e9db9c32bac47cee1
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 16:59:33 +0100

    Added ImageCarousel and ImageCarouselItem widgets

---
+++ lib/ui/shared/widgets/carousel/image_carousel/image_carousel.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/models/image/image_model.dart';
+import '../carousel_active_dot/carousel_active_dot.dart';
+import 'image_carousel_item.dart';
+
+class ImageCarousel extends StatefulWidget {
+  final List<ImageModel> images;
+  final double? height;
+
+  const ImageCarousel({super.key, required this.images, this.height});
+
+  @override
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  late final PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.height,
+      child: Stack(
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemCount: widget.images.length,
+            pageSnapping: true,
+            itemBuilder: (context, index) {
+              return ImageCarouselItem(
+                image: widget.images[index].filePath,
+              );
+            },
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List<Widget>.generate(
+                widget.images.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: InkWell(
+                    onTap: () => _controller.animateToPage(
+                      index,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeOutCubic,
+                    ),
+                    child: CircleAvatar(
+                      radius: 3,
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          CarouselActiveDot(
+            controller: _controller,
+            length: widget.images.length,
+            radius: 3,
+            padding: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/shared/widgets/carousel/image_carousel/image_carousel_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/services/image_service.dart';
+import '../../../../../utils/enums.dart';
+import '../../backdrop_image/backdrop_image.dart';
+import '../../custom_image/custom_network_image.dart';
+
+class ImageCarouselItem extends StatelessWidget {
+  final String image;
+
+  const ImageCarouselItem({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              backgroundColor: Colors.transparent,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: InteractiveViewer(
+                    maxScale: 4,
+                    child: CustomNetworkImage(
+                      placeholderIcon: const Icon(Icons.person),
+                      url: ImageService.getImageUrl(
+                        size: ProfileSizes.original.name,
+                        path: image,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+      child: BackdropImage(
+        imgUrl: image,
+        placeholderIcon: const Icon(Icons.person),
+      ),
+    );
+  }
+}
