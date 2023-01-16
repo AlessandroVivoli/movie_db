2819c118f04a5fd5f2e9e3167375866d46cf60b5
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-16 17:00:22 +0100

    Moved ActiveDot to a new file

--- lib/ui/shared/widgets/carousel/movie_carousel/movie_carousel.dart
+++ lib/ui/shared/widgets/carousel/movie_carousel/movie_carousel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/models/movie/movie.dart';
+import '../carousel_active_dot/carousel_active_dot.dart';
 import 'movie_carousel_item.dart';
 
 class MovieCarousel extends StatefulWidget {
@@ -32,18 +33,18 @@ class _MovieCarouselState extends State<MovieCarousel> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200,
+      height: widget.height,
       child: Stack(
         children: [
           PageView.builder(
             controller: _controller,
             itemCount: widget.movies.length,
             pageSnapping: true,
-            itemBuilder: (context, pagePosition) {
+            itemBuilder: (context, index) {
               return MovieCarouselItem(
-                image: widget.movies[pagePosition].backdropPath ?? '',
-                title: widget.movies[pagePosition].title!,
-                id: widget.movies[pagePosition].id,
+                image: widget.movies[index].backdropPath,
+                title: widget.movies[index].title!,
+                id: widget.movies[index].id,
               );
             },
           ),
@@ -74,68 +75,13 @@ class _MovieCarouselState extends State<MovieCarousel> {
               ),
             ),
           ),
-          _ActiveDot(
+          CarouselActiveDot(
             controller: _controller,
             length: widget.movies.length,
             radius: 3,
             padding: 5,
           ),
         ],
-      ),
-    );
-  }
-}
-
-class _ActiveDot extends StatefulWidget {
-  final PageController controller;
-  final int length;
-
-  final int? radius;
-  final int? padding;
-
-  const _ActiveDot({
-    Key? key,
-    required this.controller,
-    required this.length,
-    int? radius,
-    int? padding,
-  })  : radius = radius ?? 3,
-        padding = padding ?? 5,
-        super(key: key);
-
-  @override
-  State<_ActiveDot> createState() => _ActiveDotState();
-}
-
-class _ActiveDotState extends State<_ActiveDot> {
-  late double _activePage;
-
-  @override
-  void initState() {
-    _activePage = 0;
-
-    widget.controller.addListener(() {
-      setState(() {
-        _activePage = widget.controller.page ?? 0;
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double dotComposite = (widget.padding! * 2 + widget.radius! * 2.0);
-    double screenWidth = MediaQuery.of(context).size.width;
-    double calculatedWidth = (screenWidth - dotComposite * widget.length);
-    double startPosition = calculatedWidth / 2;
-
-    return Positioned(
-      bottom: (20 - widget.radius! * 2) / 2,
-      left: startPosition + (dotComposite * _activePage),
-      width: dotComposite,
-      child: CircleAvatar(
-        radius: widget.radius! * 1,
-        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
     );
   }
