a015f41ff46e53744aefba414367a2490cb1b4e2
Alessandro <alevivoli85@gmail.com>
2023-01-18 18:45:16 +0100

    Added AccountDetails route logic

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../core/models/account/account_details.dart';
 import '../core/models/movie/details/movie_details.dart';
 import '../core/models/person/details/person_details.dart';
 import '../core/models/video/video.dart';
-import '../ui/screens/auth/login.dart';
+import '../ui/screens/account_details/account_details_screen.dart';
+import '../ui/screens/auth/login_screen.dart';
 import '../ui/screens/home/home_screen.dart';
 import '../ui/screens/movie_details/movie_details_screen.dart';
 import '../ui/screens/person_details/person_details_screen.dart';
@@ -12,31 +14,56 @@ import 'routes.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
+    // HomeScreen
     case AppRoute.home:
       return MaterialPageRoute(
         builder: (context) => const HomeScreen(title: 'FlutterMovie'),
       );
+
+    // MovieDetailsScreen
     case AppRoute.movie:
       final movieDetails = settings.arguments as Future<MovieDetails>;
 
       return MaterialPageRoute(
-        builder: (context) => MovieDetailsScreen(movieDetails: movieDetails),
+        builder: (context) => MovieDetailsScreen(
+          movieDetails: movieDetails,
+        ),
       );
+
+    // PersonDetailsScreen
     case AppRoute.person:
       final personDetails = settings.arguments as Future<PersonDetails>;
 
       return MaterialPageRoute(
-        builder: (context) => PersonDetailsScreen(personDetails: personDetails),
+        builder: (context) => PersonDetailsScreen(
+          personDetails: personDetails,
+        ),
       );
+
+    // VideoScreen
     case AppRoute.play:
       final videoList = settings.arguments as Future<List<Video>>;
 
       return MaterialPageRoute(
-        builder: (context) => VideoScreen(videoList: videoList),
+        builder: (context) => VideoScreen(
+          videoList: videoList,
+        ),
       );
+
+    // LoginScreen
     case AppRoute.login:
       return MaterialPageRoute(
         builder: (context) => const LoginScreen(),
+      );
+
+    // AccountDetailsScreen
+    case AppRoute.account:
+      final accountDetails = settings.arguments as AccountDetails;
+
+      return MaterialPageRoute(
+        builder: (context) => AccountDetailsScreen(
+          accountDetails: accountDetails,
+        ),
       );
     default:
       throw Exception('Route ${settings.name} doesn\'t exist');
