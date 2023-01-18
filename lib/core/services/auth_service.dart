f360299ca1a2fb9a94c608078ea63f95bd4bd176
Alessandro <alevivoli85@gmail.com>
2023-01-18 14:18:08 +0100

    Added login method to AuthService

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,3 +1,6 @@
+import 'package:dio/dio.dart';
+
+import '../models/auth/login_response.dart';
 import '../models/auth/request_token.dart';
 import '../models/auth/session_id.dart';
 import '../providers/dio_provider.dart';
@@ -40,6 +43,67 @@ class AuthService {
         )
         .then((res) => Map<String, Object?>.from(res.data))
         .then(SessionId.fromJson);
+  }
+
+  static Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async {
+    bool success = true;
+    int statusCode = 0;
+    String message = '';
+    String? sessionId;
+
+    final token = await AuthService.createRequestToken().catchError((_) {
+      message = 'Could not get request token.';
+
+      return const RequestToken(
+        success: false,
+        expiresAt: '',
+        requestToken: '',
+      );
+    });
+
+    if (token.success) {
+      final validatedToken = await AuthService.validateTokenWithLogin(
+        requestToken: token.requestToken,
+        username: username,
+        password: password,
+      ).catchError((error) {
+        success = false;
+        statusCode = 2;
+        message = 'Could not get session id';
+
+        if (error is DioError && error.response?.statusCode == 401) {
+          message = 'Wrong username and/or password';
+        }
+
+        return const RequestToken(
+          success: false,
+          expiresAt: '',
+          requestToken: '',
+        );
+      });
+
+      if (validatedToken.success) {
+        final session = await AuthService.createSessionId(
+          requestToken: validatedToken.requestToken,
+        ).catchError((_) {
+          return const SessionId(success: false, sessionId: '');
+        });
+
+        if (session.success) {
+          sessionId = session.sessionId;
+        }
+      }
+    }
+
+    return LoginResponse(
+      success: success,
+      statusCode: statusCode,
+      message: message,
+      sessionId: sessionId,
+    );
   }
 
   static Future<bool> logout({required String sessionId}) {
