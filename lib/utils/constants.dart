2e5bb4f5c4d2feaeb25e1110f9dc6a5132f5e7a0
Alessandro <alevivoli85@gmail.com>
2023-01-19 13:39:17 +0100

    Added SortBy constant

---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+class SortBy {
+  static const popularityAscending = 'popularity.asc';
+  static const popularityDescending = 'popularity.desc';
+  static const releaseDateAscending = 'release_date.asc';
+  static const releaseDateDescending = 'release_date.desc';
+  static const revenueAscending = 'revenue.asc';
+  static const revenueDescending = 'revenue.desc';
+  static const primaryReleaseDateAscending = 'primary_release_date.asc';
+  static const primaryReleaseDateDescending = 'primary_release_date.desc';
+  static const originalTitleAscending = 'original_title.asc';
+  static const originalTitleDescending = 'original_title.desc';
+  static const voteAverageAscending = 'vote_average.asc';
+  static const voteAverageDescending = 'vote_average.desc';
+  static const voteCountAscending = 'vote_count.asc';
+  static const voteCountDescending = 'vote_count.desc';
+}
