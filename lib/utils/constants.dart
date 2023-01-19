a0cb3eefb8c03082d37c5bc7d9225a8aa30efec9
Alessandro <alevivoli85@gmail.com>
2023-01-19 13:51:55 +0100

    Added TimeWindow constant - Changed SortBy constant to use class constants instead of string

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,16 +1,40 @@
 class SortBy {
-  static const popularityAscending = 'popularity.asc';
-  static const popularityDescending = 'popularity.desc';
-  static const releaseDateAscending = 'release_date.asc';
-  static const releaseDateDescending = 'release_date.desc';
-  static const revenueAscending = 'revenue.asc';
-  static const revenueDescending = 'revenue.desc';
-  static const primaryReleaseDateAscending = 'primary_release_date.asc';
-  static const primaryReleaseDateDescending = 'primary_release_date.desc';
-  static const originalTitleAscending = 'original_title.asc';
-  static const originalTitleDescending = 'original_title.desc';
-  static const voteAverageAscending = 'vote_average.asc';
-  static const voteAverageDescending = 'vote_average.desc';
-  static const voteCountAscending = 'vote_count.asc';
-  static const voteCountDescending = 'vote_count.desc';
+  final String _value;
+
+  static const popularityAsc = SortBy._('popularity.asc');
+  static const popularityDesc = SortBy._('popularity.desc');
+  static const releaseDateAsc = SortBy._('release_date.asc');
+  static const releaseDateDesc = SortBy._('release_date.desc');
+  static const revenueAsc = SortBy._('revenue.asc');
+  static const revenueDesc = SortBy._('revenue.desc');
+  static const primaryReleaseDateAsc = SortBy._('primary_release_date.asc');
+  static const primaryReleaseDateDesc = SortBy._('primary_release_date.desc');
+  static const originalTitleAsc = SortBy._('original_title.asc');
+  static const originalTitleDesc = SortBy._('original_title.desc');
+  static const voteAverageAsc = SortBy._('vote_average.asc');
+  static const voteAverageDesc = SortBy._('vote_average.desc');
+  static const voteCountAsc = SortBy._('vote_count.asc');
+  static const voteCountDesc = SortBy._('vote_count.desc');
+
+  const SortBy._(String value) : _value = value;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return _value;
+  }
+}
+
+class TimeWindow {
+  final String _value;
+
+  static const week = TimeWindow._('week');
+  static const day = TimeWindow._('day');
+
+  const TimeWindow._(String value) : _value = value;
+
+  @override
+  String toString() {
+    return _value;
+  }
 }
