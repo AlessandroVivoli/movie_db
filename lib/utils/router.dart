2a6d3ec82ce7ae9edd7eb0054abdc709e86af338
Alessandro <alevivoli85@gmail.com>
2023-01-20 14:33:39 +0100

    Changed to pass movieId instead of  MovieDetails

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../core/models/account/account_details.dart';
-import '../core/models/movie/details/movie_details.dart';
 import '../core/models/person/details/person_details.dart';
 import '../core/models/video/video.dart';
 import '../ui/screens/account_details/account_details_screen.dart';
@@ -22,11 +21,11 @@ Route<dynamic> generateRoute(RouteSettings settings) {
 
     // MovieDetailsScreen
     case AppRoute.movie:
-      final movieDetails = settings.arguments as Future<MovieDetails>;
+      final movieId = settings.arguments as int;
 
       return MaterialPageRoute(
         builder: (context) => MovieDetailsScreen(
-          movieDetails: movieDetails,
+          movieId: movieId,
         ),
       );
 
