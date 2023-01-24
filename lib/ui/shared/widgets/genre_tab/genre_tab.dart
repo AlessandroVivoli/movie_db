c8c841b6897debfba55c19ede848b4096e47c57b
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-24 17:43:32 +0100

    Updated GenreTab widget to ConsumerWidget - Updated to use FutureProvider instead of FutureBuilder

--- lib/ui/shared/widgets/genre_tab/genre_tab.dart
+++ lib/ui/shared/widgets/genre_tab/genre_tab.dart
@@ -1,51 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loggy/loggy.dart';
 
 import '../../../../core/services/genre_service.dart';
 import '../errors/error_snack_bar_content.dart';
 import '../errors/error_text.dart';
 import 'widgets/genre_tab_controller.dart';
 
-class GenreTab extends StatelessWidget {
+final _genreFutureProvider = FutureProvider.autoDispose(
+  (ref) => GenreService.getGenres(),
+);
+
+class GenreTab extends ConsumerWidget {
   const GenreTab({super.key, this.includeAdult});
 
   final bool? includeAdult;
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: GenreService.getGenres(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          if (snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text('Nothing found.'),
-            );
-          }
+  Widget build(BuildContext context, WidgetRef ref) {
+    final genres = ref.watch(_genreFutureProvider);
 
-          final data = snapshot.data!;
-
-          return GenreTabController(data: data);
-        } else if (snapshot.hasError) {
-          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: Theme.of(context).colorScheme.surface,
-                content: const ErrorSnackBarContent(
-                  message: 'Could not get genres.',
-                ),
-              ),
-            );
-          });
-
+    return genres.when(
+      data: (genreList) {
+        if (genreList.isEmpty) {
           return const Center(
-            child: ErrorText('Something went wrong.'),
+            child: Text('Nothing found.'),
           );
         }
 
+        return GenreTabController(data: genreList);
+      },
+      error: (error, stackTrace) {
+        logError('Could not get genres.', error, stackTrace);
+
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              content: const ErrorSnackBarContent(
+                message: 'Could not get genres.',
+              ),
+            ),
+          );
+        });
+
         return const Center(
-          child: CircularProgressIndicator(),
+          child: ErrorText('Something went wrong.'),
         );
       },
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
