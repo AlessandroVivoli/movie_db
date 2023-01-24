d06c9affaa088775754db16a32568086ba291a96
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-24 17:36:55 +0100

    Added AccountDetails State provider

---
+++ lib/core/providers/general_providers.dart
@@ -0,0 +1,6 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../models/account/account_details.dart';
+
+final accountDetailsStateProvider =
+    StateProvider<AccountDetails?>((ref) => null);
