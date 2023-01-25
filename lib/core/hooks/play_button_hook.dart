9cb7c38f2c7d80026ef9ed0b84f4526825552925
AlessandroVivoli <alevivoli85@gmail.com>
2023-01-25 12:54:34 +0100

    Added custom PlayButton hook

---
+++ lib/core/hooks/play_button_hook.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+import '../models/play_button/play_button_scale.dart';
+
+ValueNotifier<PlayButtonScale> usePlayButtonScale(
+    ScrollController controller, double offset) {
+  const double defaultTopMargin = 260;
+
+  const double scaleStart = (defaultTopMargin - 16) / 2;
+  const double scaleEnd = scaleStart / 2;
+
+  double top = defaultTopMargin;
+  double scale = 1;
+
+  if (controller.hasClients) {
+    top -= offset;
+
+    if (offset < defaultTopMargin - scaleStart) {
+      scale = 1;
+    } else if (offset < defaultTopMargin - scaleEnd) {
+      scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+    } else {
+      scale = 0;
+    }
+  }
+
+  return ValueNotifier(
+    PlayButtonScale(
+      top: top,
+      offset: offset,
+      scale: scale,
+    ),
+  );
+}
