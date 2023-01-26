baf206ee15923fd2f8605673454ab4cbc3375e6f
Alessandro <alevivoli85@gmail.com>
2023-01-26 18:06:32 +0100

    Added generic type IAuthService to Provider

--- lib/core/providers/auth_provider.dart
+++ lib/core/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../interfaces/i_auth_service.dart';
 import '../services/auth_service.dart';
 
-final authServiceProvider = Provider((ref) => AuthService());
+final authServiceProvider = Provider<IAuthService>((ref) => AuthService());
