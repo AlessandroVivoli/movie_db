870d2a657e036d25e94c41b2f6d96749e2bb9db3
Alessandro <alevivoli85@gmail.com>
2023-01-28 18:19:32 +0100

    Updated genreProvider

--- lib/core/providers/genre_provider.dart
+++ lib/core/providers/genre_provider.dart
@@ -2,8 +2,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../interfaces/i_genre_service.dart';
 import '../services/genre_service.dart';
+import 'dio_provider.dart';
 
-final genreServiceProvider = Provider<IGenreService>((ref) => GenreService());
+final genreServiceProvider = Provider<IGenreService>(
+  (ref) => GenreService(ref.read(dioProvider)),
+);
 
 final getGenresProvider =
     FutureProvider((ref) => ref.watch(genreServiceProvider).getGenres());
