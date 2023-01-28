377bf1808f79bc1ca0eb853a99047727c79b4cdd
Alessandro <alevivoli85@gmail.com>
2023-01-28 18:19:05 +0100

    Added AuthNotifier and AuthProvider

---
+++ lib/core/notifiers/auth_notifier.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../error/invalid_user_error.dart';
+import '../models/user/user.dart';
+import '../providers/account_service_provider.dart';
+import '../providers/auth_provider.dart';
+import '../providers/local_storage_provider.dart';
+import '../providers/user_provider.dart';
+import '../unions/auth_state.dart';
+
+class AuthNotifier extends Notifier<AuthState> {
+  Future<void> login(String username, String password) async {
+    state = const AuthState.loading();
+
+    final sessionId = await ref
+        .read(authServiceProvider)
+        .login(
+          username: username,
+          password: password,
+        )
+        .catchError((e) => _handleLoginError(e));
+
+    if (sessionId != null) {
+      ref.read(localStorageProvider)?.setSessionId(sessionId);
+
+      final accountDetails = await ref
+          .read(accountServiceProvider)
+          .getAccountDetails(sessionId: sessionId);
+
+      state = AuthState.loggedIn(User(
+        accountDetails: accountDetails,
+        sessionId: sessionId,
+      ));
+    }
+  }
+
+  Future<void> logout() async {
+    state = const AuthState.loading();
+
+    final sessionId = ref.read(localStorageProvider)?.getSessionId();
+
+    bool success = false;
+    if (sessionId != null) {
+      success = await ref.read(authServiceProvider).logout(
+            sessionId: sessionId,
+          );
+    }
+
+    if (!success) {
+      state = AuthState.error(
+        Exception('Could not logout'),
+        StackTrace.current,
+      );
+
+      return;
+    }
+
+    ref.read(localStorageProvider)?.setSessionId(null);
+    ref.read(userProvider.notifier).state = null;
+
+    state = const AuthState.loggedOut();
+  }
+
+  String? _handleLoginError(Object e) {
+    final err = e as DioError;
+
+    if (err.response!.data['status_code'] == 30) {
+      final invalidUserError = InvalidUserError(
+        error: err.error,
+        stackTrace: err.stackTrace!,
+      );
+
+      state = AuthState.error(
+        invalidUserError.error,
+        invalidUserError.stackTrace,
+      );
+    } else {
+      state = AuthState.error(err.error, err.stackTrace!);
+    }
+
+    return null;
+  }
+
+  @override
+  AuthState build() {
+    return const AuthState.loggedOut();
+  }
+}
--- lib/core/providers/auth_provider.dart
+++ lib/core/providers/auth_provider.dart
@@ -1,6 +1,15 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../interfaces/i_auth_service.dart';
+import '../notifiers/auth_notifier.dart';
 import '../services/auth_service.dart';
+import '../unions/auth_state.dart';
+import 'dio_provider.dart';
 
-final authServiceProvider = Provider<IAuthService>((ref) => AuthService());
+final authServiceProvider = Provider<IAuthService>(
+  (ref) => AuthService(ref.read(dioProvider)),
+);
+
+final authProvider = NotifierProvider<AuthNotifier, AuthState>(() {
+  return AuthNotifier();
+});
