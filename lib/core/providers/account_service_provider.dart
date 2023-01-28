3583c57fbbad901fa0d3503486e7244cf08d2aaf
Alessandro <alevivoli85@gmail.com>
2023-01-28 18:18:23 +0100

    Updated accountServiceProvider

--- lib/core/providers/account_service_provider.dart
+++ lib/core/providers/account_service_provider.dart
@@ -4,9 +4,10 @@ import '../interfaces/i_account_avatar_service.dart';
 import '../interfaces/i_account_service.dart';
 import '../services/account_avatar_service.dart';
 import '../services/account_service.dart';
+import 'dio_provider.dart';
 
 final accountServiceProvider =
-    Provider<IAccountService>((ref) => AccountService());
+    Provider<IAccountService>((ref) => AccountService(ref.read(dioProvider)));
 
 final accountAvatarServiceProvider =
     Provider<IAccountAvatarService>((ref) => AccountAvatarService());
