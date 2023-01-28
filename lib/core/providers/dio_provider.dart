a0e55413fa341f4c67570aaab1019ec4e5408b69
Alessandro <alevivoli85@gmail.com>
2023-01-28 18:19:20 +0100

    Updated dioProvider

--- lib/core/providers/dio_provider.dart
+++ lib/core/providers/dio_provider.dart
@@ -1,27 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../utils/interceptors/query_interceptor.dart';
 
-class DioProvider {
-  static Dio? _dio;
-
-  static Dio _init() {
-    return Dio(
-      BaseOptions(baseUrl: dotenv.get('BASE_URL')),
-    )..interceptors.addAll(
-        [
-          QueryInterceptor(),
-          if (!kDebugMode) PrettyDioLogger(),
-        ],
-      );
-  }
-
-  static Dio get dio {
-    _dio ??= _init();
-
-    return _dio!;
-  }
-}
+final dioProvider = Provider(
+  (ref) => Dio(
+    BaseOptions(baseUrl: dotenv.get('BASE_URL')),
+  )..interceptors.addAll(
+      [
+        QueryInterceptor(),
+        if (!kDebugMode) PrettyDioLogger(),
+      ],
+    ),
+);
