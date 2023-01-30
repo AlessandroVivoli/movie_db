db7dd5800358c2a12d08a8d569bd7a0ba239258b
Alessandro <alevivoli85@gmail.com>
2023-01-30 11:55:42 +0100

    Moved both .then methods outside for readability

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -12,26 +12,27 @@ class AuthService implements IAuthService {
     required String username,
     required String password,
   }) async {
-    return _dio.get('/authentication/token/new').then(
-          (res) => _dio
-              .post(
-                '/authentication/token/validate_with_login',
-                data: {
-                  'username': username,
-                  'password': password,
-                  'request_token': res.data['request_token'],
-                },
-              )
-              .then(
-                (res) => _dio.post(
-                  '/authentication/session/new',
-                  data: {
-                    'request_token': res.data['request_token'],
-                  },
-                ),
-              )
-              .then((res) => res.data['session_id']),
-        );
+    return _dio
+        .get('/authentication/token/new')
+        .then(
+          (res) => _dio.post(
+            '/authentication/token/validate_with_login',
+            data: {
+              'username': username,
+              'password': password,
+              'request_token': res.data['request_token'],
+            },
+          ),
+        )
+        .then(
+          (res) => _dio.post(
+            '/authentication/session/new',
+            data: {
+              'request_token': res.data['request_token'],
+            },
+          ),
+        )
+        .then((res) => res.data['session_id']);
   }
 
   @override
