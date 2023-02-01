b7ac100a1bd0834cf55acc77b780838a7e8afb48
AlessandroVivoli <alevivoli85@gmail.com>
2023-02-01 18:29:41 +0100

    Changed HomeScreen to StatelessWidget

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../core/models/user/user.dart';
-import '../../../core/providers/account/account_service_provider.dart';
-import '../../../core/providers/auth/auth_provider.dart';
-import '../../../core/providers/local_storage/local_storage_provider.dart';
 import '../../../core/providers/movie/get_trending_movies_provider.dart';
-import '../../../core/unions/auth_state.dart';
 import '../../../utils/constants.dart';
 import '../../../utils/extensions.dart';
 import '../../../utils/routes.dart';
@@ -19,24 +13,14 @@ import '../../shared/widgets/genre_tab/genre_tab.dart';
 import '../../shared/widgets/search/custom_search_delegate.dart';
 import 'home_wrapper/home_wrapper.dart';
 
-class HomeScreen extends HookConsumerWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
-
-    useEffect(() {
-      final sessionId = ref.read(localStorageProvider).getSessionId();
-
-      if (sessionId != null) {
-        getUser(ref, sessionId, context);
-      }
-
-      return null;
-    }, []);
 
     return SafeArea(
       top: false,
@@ -66,24 +50,6 @@ class HomeScreen extends HookConsumerWidget {
         ),
       ),
     );
-  }
-
-  void getUser(WidgetRef ref, String sessionId, BuildContext context) async {
-    ref
-        .read(accountServiceProvider)
-        .getAccountDetails(sessionId: sessionId)
-        .then(
-      (accountDetails) {
-        ref.read(authProvider.notifier).state = AuthState.loggedIn(
-          User(
-            accountDetails: accountDetails,
-            sessionId: sessionId,
-          ),
-        );
-      },
-    ).catchError((_) {
-      context.showErrorSnackBar('Could not get user.');
-    });
   }
 }
 
