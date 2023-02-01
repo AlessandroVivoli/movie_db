606616ab4cd47e20272da83b6a81cf07af7e62a0
AlessandroVivoli <alevivoli85@gmail.com>
2023-02-01 14:39:58 +0100

    Removed RefreshType enum

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -5,5 +5,3 @@ enum LogoSizes { w45, w92, w154, w185, w300, w500, original }
 enum PosterSizes { w92, w154, w185, w342, w500, w780, original }
 
 enum ProfileSizes { w45, w185, h632, original }
-
-enum RefreshType { watchlist, rate, favorites }
