c302256cae4bc59997b975b39889a02ee72d69f9
AlessandroVivoli <alevivoli85@gmail.com>
2023-02-02 21:09:33 +0100

    Added splash screen to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loggy/loggy.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'core/loggers/provider_logger.dart';
 import 'features/auth/provider/auth_provider.dart';
 import 'features/local_storage/data/local_storage_repository.dart';
 import 'features/local_storage/provider/local_storage_provider.dart';
 import 'routing/router.dart';
 import 'routing/routes.dart';
 import 'themes/main_theme.dart';
-import 'core/loggers/provider_logger.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await dotenv.load();
 
   final prefs = await SharedPreferences.getInstance();
@@ -40,30 +45,30 @@ void main() async {
 
   final container = ProviderContainer(
     overrides: [
-      localStorageProvider.overrideWithValue(
-        LocalStorageRepository(prefs),
-      )
+      localStorageProvider.overrideWithValue(LocalStorageRepository(prefs))
+    ],
+    observers: [
+      ProviderLogger(),
     ],
   );
 
   await container.read(authProvider.notifier).init();
 
   runApp(
-    ProviderScope(
-      observers: [
-        ProviderLogger(),
-      ],
-      parent: container,
+    UncontrolledProviderScope(
+      container: container,
       child: const MyApp(),
     ),
   );
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       color: const Color.fromARGB(255, 17, 25, 37),
