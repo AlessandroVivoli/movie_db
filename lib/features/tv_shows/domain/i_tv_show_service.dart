9d5b4e2a50d52b8861c7e097232ce7ce46bc5c73
AlessandroVivoli <alevivoli85@gmail.com>
2023-02-09 19:48:22 +0100

    Added TVShowService

--- lib/features/tv_shows/domain/i_tv_show_service.dart
+++ lib/features/tv_shows/domain/i_tv_show_service.dart
@@ -1,8 +1,12 @@
+import '../../account/domain/account_media_status/account_media_status.dart';
 import '../../movies/domain/sort_by.dart';
 import '../../time_window/domain/time_window.dart';
 import 'tv_show.dart';
+import 'tv_show_details.dart';
 
 abstract class ITVShowService {
+  const ITVShowService();
+
   Future<List<TVShow>> getTrendingTVShows({
     TimeWindow timeWindow = TimeWindow.week,
   });
@@ -12,6 +16,16 @@ abstract class ITVShowService {
   Future<List<TVShow>> getTVShows({
     List<int>? withGenres,
     SortBy sortBy = SortBy.popularityDesc,
-    bool includeAdult = false,
   });
+
+  Future<AccountMediaStatus> getTVAccountStatus({required int id});
+
+  Future<void> rateTV({
+    required int id,
+    required double rating,
+  });
+
+  Future<void> deleteTVRating({required int id});
+
+  Future<TVShowDetails> getTVShowDetails({required int id});
 }
