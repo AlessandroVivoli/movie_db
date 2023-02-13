64d4947765e2e531c5fcdf0bbf0c26c7b93cc343
Alessandro <alevivoli85@gmail.com>
2023-02-13 13:37:09 +0100

    Added tv search delegate to TVHomePage

--- lib/ui/home/tv_home_page/tv_home_page.dart
+++ lib/ui/home/tv_home_page/tv_home_page.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../core/extensions/build_context_extensions.dart';
+import '../../../core/widgets/carousel/tv_carousel/tv_carousel.dart';
+import '../../../core/widgets/errors/error_text.dart';
+import '../../../core/widgets/genre_tab/tv_genre_tab.dart';
+import '../../../core/widgets/search/tv_show_search_delegate.dart';
+import '../../../features/time_window/domain/time_window.dart';
+import '../../../features/tv_shows/provider/get_trending_tvs_provider.dart';
+import '../../../routing/routes.dart';
+import 'tvs_home_wrapper/tvs_home_wrapper.dart';
 
 class TVHomePage extends StatelessWidget {
   const TVHomePage({super.key, required this.title});
@@ -10,8 +23,11 @@ class TVHomePage extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         _AppBar(title: title),
-        const SliverFillRemaining(
-          child: Placeholder(),
+        const SliverToBoxAdapter(
+          child: TVGenreTab(),
+        ),
+        const SliverToBoxAdapter(
+          child: TVsHomeWrapper(),
         ),
       ],
     );
@@ -28,11 +44,68 @@ class _AppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
       pinned: true,
       expandedHeight: 200,
+      actions: [
+        IconButton(
+          onPressed: () async {
+            final tvId = await showSearch(
+              context: context,
+              delegate: TVShowSearchDelegate(),
+            );
+
+            if (tvId != null) {
+              WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                Navigator.pushNamed(context, AppRoute.tv, arguments: tvId);
+              });
+            }
+          },
+          icon: const Icon(Icons.search),
+        )
+      ],
       title: Text(title),
       centerTitle: true,
-      flexibleSpace: const FlexibleSpaceBar(),
+      flexibleSpace: const FlexibleSpaceBar(
+        background: _TrendingTVsCarousel(),
+      ),
+    );
+  }
+}
+
+class _TrendingTVsCarousel extends ConsumerWidget {
+  const _TrendingTVsCarousel();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final trendingTvs = ref.watch(getTrendingTVShowsProvider(TimeWindow.week));
+
+    final localization = AppLocalizations.of(context)!;
+
+    return trendingTvs.when(
+      data: (tvs) {
+        final tvList = tvs.take(6).toList();
+
+        if (tvList.isEmpty) {
+          return const Center(
+            child: Text('No trending tv show found.'),
+          );
+        }
+
+        return TVCarousel(tvs: tvList);
+      },
+      error: (error, stackTrace) {
+        context.showErrorSnackBar('Could not get trending tv shows');
+
+        return Center(
+          child: ErrorText(localization.unexpectedErrorMessage),
+        );
+      },
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
