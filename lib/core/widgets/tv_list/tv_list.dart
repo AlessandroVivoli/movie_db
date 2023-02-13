56ae9ba56a376a1a78756d0a1ab3aab3d695ae9d
Alessandro <alevivoli85@gmail.com>
2023-02-13 13:09:02 +0100

    Added TVList widget

---
+++ lib/core/widgets/tv_list/tv_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../../features/tv_shows/domain/tv_show.dart';
+import '../tv_card/tv_card.dart';
+
+class TVList extends StatelessWidget {
+  final List<TVShow> tvList;
+  final double padding;
+  final ScrollController? controller;
+  final double seperatorSize;
+
+  const TVList({
+    super.key,
+    required this.tvList,
+    this.padding = 0,
+    this.controller,
+    this.seperatorSize = 10,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      controller: controller,
+      scrollDirection: Axis.horizontal,
+      itemCount: tvList.length,
+      padding: EdgeInsets.symmetric(horizontal: padding),
+      separatorBuilder: (context, index) {
+        return SizedBox(width: seperatorSize);
+      },
+      itemBuilder: (context, index) {
+        return TVCard(tvShow: tvList[index]);
+      },
+    );
+  }
+}
