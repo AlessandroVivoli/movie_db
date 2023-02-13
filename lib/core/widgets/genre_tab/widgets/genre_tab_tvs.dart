0d66f85e0dd6ecf0eef1db0cefa3491af87cde27
Alessandro <alevivoli85@gmail.com>
2023-02-13 13:01:53 +0100

    Added GenreTabTVs widget

---
+++ lib/core/widgets/genre_tab/widgets/genre_tab_tvs.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../../features/media/domain/media_arguments.dart';
+import '../../../../features/tv_shows/provider/get_tv_shows_provider.dart';
+import '../../../extensions/build_context_extensions.dart';
+import '../../errors/error_text.dart';
+import '../../tv_list/tv_list.dart';
+
+class GenreTabTvs extends ConsumerWidget {
+  const GenreTabTvs({
+    super.key,
+    this.includeAdult = false,
+    required this.genreId,
+  });
+
+  final bool includeAdult;
+  final int genreId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tvList = ref.watch(
+      getTVShowsProvider(
+        MediaArguments(
+          withGenres: [genreId],
+          includeAdult: includeAdult,
+        ),
+      ),
+    );
+
+    final localization = AppLocalizations.of(context)!;
+
+    return tvList.when(
+      data: (tvs) {
+        if (tvs.isEmpty) {
+          return const Center(
+            child: Text('No popular tv show found.'),
+          );
+        }
+
+        return TVList(
+          tvList: tvs,
+          padding: 10,
+        );
+      },
+      error: (error, stackTrace) {
+        context.showErrorSnackBar('Could not get popular tvs');
+
+        return Center(
+          child: ErrorText(localization.unexpectedErrorMessage),
+        );
+      },
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
