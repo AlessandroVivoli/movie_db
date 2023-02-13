b9ac00576db752eac69deef4ac27e5c1f53da9a0
Alessandro <alevivoli85@gmail.com>
2023-02-13 13:01:20 +0100

    Added includeAdult for MovieGenreTab

--- lib/core/widgets/genre_tab/movie_genre_tab.dart
+++ lib/core/widgets/genre_tab/movie_genre_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../features/auth/provider/auth_provider.dart';
 import '../../../features/genre/provider/get_movie_genres_provider.dart';
 import '../../extensions/build_context_extensions.dart';
 import '../errors/error_text.dart';
@@ -14,6 +15,11 @@ class MovieGenreTab extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final genres = ref.watch(getMovieGenresProvider);
 
+    final includeAdult = ref.watch(authProvider).maybeWhen(
+          loggedIn: (user) => user.accountDetails.includeAdult,
+          orElse: () => false,
+        );
+
     final localization = AppLocalizations.of(context)!;
 
     return genres.when(
@@ -24,7 +30,11 @@ class MovieGenreTab extends ConsumerWidget {
           );
         }
 
-        return GenreTabController(data: genreList);
+        return GenreTabController(
+          data: genreList,
+          mediaType: 'movie',
+          includeAdult: includeAdult,
+        );
       },
       error: (error, stackTrace) {
         context.showErrorSnackBar(localization.getMovieGenresError);
