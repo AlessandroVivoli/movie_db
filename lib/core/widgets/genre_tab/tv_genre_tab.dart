cadc51adf287f026ccd7c21d8e83fad0d3720585
Alessandro <alevivoli85@gmail.com>
2023-02-13 13:01:34 +0100

    Added IncludeAdult for tv_genre_tab

--- lib/core/widgets/genre_tab/tv_genre_tab.dart
+++ lib/core/widgets/genre_tab/tv_genre_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../features/auth/provider/auth_provider.dart';
 import '../../../features/genre/provider/get_tv_genres_provider.dart';
 import '../../extensions/build_context_extensions.dart';
 import 'widgets/genre_tab_controller.dart';
@@ -13,25 +14,37 @@ class TVGenreTab extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final genres = ref.watch(getTVGenresProvider);
 
+    final includeAdult = ref.watch(authProvider).maybeWhen(
+          loggedIn: (user) => user.accountDetails.includeAdult,
+          orElse: () => false,
+        );
+
     final localization = AppLocalizations.of(context)!;
 
-    return genres.when(
-      data: (genreList) {
-        if (genreList.isEmpty) {
-          return Center(child: Text(localization.noTVGenres));
-        }
+    return SizedBox(
+      height: 300,
+      child: genres.when(
+        data: (genreList) {
+          if (genreList.isEmpty) {
+            return Center(child: Text(localization.noTVGenres));
+          }
 
-        return GenreTabController(data: genreList);
-      },
-      error: (error, stackTrace) {
-        context.showErrorSnackBar(localization.getTVGenresError);
+          return GenreTabController(
+            data: genreList,
+            mediaType: 'tv',
+            includeAdult: includeAdult,
+          );
+        },
+        error: (error, stackTrace) {
+          context.showErrorSnackBar(localization.getTVGenresError);
 
-        return Center(
-          child: Text(localization.unexpectedErrorMessage),
-        );
-      },
-      loading: () => const Center(
-        child: CircularProgressIndicator(),
+          return Center(
+            child: Text(localization.unexpectedErrorMessage),
+          );
+        },
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
